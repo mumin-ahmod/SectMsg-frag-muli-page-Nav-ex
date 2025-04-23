5081c25282295e70c1b87b82f14f19c737c9778b
mumin-ahmod <mumin.ahmod.ledp@gmail.com>
2025-04-23 13:29:42 +0600

    2nd screen navigation done

--- app/src/main/java/com/example/sectmsg_frag_muli_page_ex/EncryptFagment.kt
+++ app/src/main/java/com/example/sectmsg_frag_muli_page_ex/EncryptFagment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentMessage.newInstance] factory method to
+ * Use the [EncryptFagment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentMessage : Fragment() {
+class EncryptFagment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,15 +34,7 @@ class FragmentMessage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_message, container, false)
-
-        val nextButton = view.findViewById<Button>(R.id.next)
-
-        nextButton.setOnClickListener{
-            view.findNavController().navigate(R.id.action_fragmentMessage_to_encryptFagment)
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_encrypt_fagment, container, false)
     }
 
     companion object {
@@ -54,12 +44,12 @@ class FragmentMessage : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentMessage.
+         * @return A new instance of fragment EncryptFagment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentMessage().apply {
+            EncryptFagment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
